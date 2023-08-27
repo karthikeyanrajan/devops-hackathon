38de3d14221e98ee5ec6f3d7f47b5f49aaadf410
karthikeyanrajan <138673317+karthikeyanrajan@users.noreply.github.com>
2023-08-27 15:33:54 +0530

    Update security_group.tf

--- security_group.tf
+++ security_group.tf
@@ -15,6 +15,21 @@ resource "aws_security_group" "karthi-sg" {
         cidr_blocks = ["0.0.0.0/0"]
 
     }
+
+ingress {
+        from_port   = 8080
+        to_port     = 8080
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]  # Allow port 8080 from any IP
+    }
+
+    ingress {
+        from_port   = 5000
+        to_port     = 5000
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]  # Allow port 5000 from any IP
+    }
+
     egress {
         from_port = 0
         to_port = 0
